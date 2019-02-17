2177f56654a33acb5e9c8eb976e6033b1209fac0
Heijden <rick.van.der.heijden@philips.com>
2019-02-17 21:07:23 +0100

    Initial commit

---
+++ src/com/travelagency/BackEndHost.java
@@ -0,0 +1,9 @@
+package com.travelagency;
+
+public class BackEndHost {
+
+    public static void main(String[] args)
+    {
+        System.out.println("TravelAgency BackEnd is running");
+    }
+}
