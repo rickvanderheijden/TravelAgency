8688275b58c91c29b0f53c2f7c9ee3db0dbc5429
Rick van der Heijden <rickvanderheijden@hotmail.com>
2019-04-02 19:36:10 +0200

    Rename Service to Controller

---
+++ src/main/java/com/travelagency/rest/TripResource.java
@@ -0,0 +1,53 @@
+package com.travelagency.rest;
+
+import com.travelagency.controllers.TripController;
+import com.travelagency.model.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/trips")
+public class TripResource {
+
+    private final TripController tripController;
+
+    @Autowired
+    public TripResource(TripController tripController) {
+        this.tripController = tripController;
+    }
+
+    @RequestMapping(value = "/createTrip", method = RequestMethod.POST)
+    public Optional<Trip> createTrip(@Valid @RequestBody Trip trip) {
+        return tripController.createTrip(trip);
+    }
+
+    @RequestMapping(value = "/updateTrip", method = RequestMethod.PUT)
+    public ResponseEntity<Trip> updateTrip(@Valid @RequestBody Trip trip) {
+        Trip updatedTrip = this.tripController.updateTrip(trip);
+        if(updatedTrip == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedTrip);
+    }
+
+    @RequestMapping(value = "/deleteTrip/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Trip> deleteTrip(@PathVariable long id) {
+        boolean isDeleted = this.tripController.deleteTrip(id);
+        if(!isDeleted){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Trip> getById(@PathVariable long id) {
+        Trip foundedTrip = this.tripController.getById(id);
+        if(foundedTrip == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(foundedTrip);
+    }
+}
