c9bad2168d80331b98b32bc3d5fb67fbe3bffe44
Koen Sengers <k.sengers@gynzy.com>
2019-05-28 21:12:46 +0200

    Add country endpoint

--- src/main/java/com/travelagency/repository/CountryRepository.java
+++ src/main/java/com/travelagency/repository/CountryRepository.java
@@ -4,9 +4,12 @@ import com.travelagency.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
     Country findByName(String name);
     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN 'true' ELSE 'false' END FROM Country c WHERE c.name = ?1")
     boolean existsByName(String name);
+    List<Country> findByContinent_Name(String continentName);
 }
