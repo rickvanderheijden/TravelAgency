6f62dd8f7172ca37cc1ef9f59647ed2a6fceb77c
Rick van der Heijden <rick.van.der.heijden@philips.com>
2019-05-31 11:44:34 +0200

    Fix Codacy issue

--- src/main/java/com/travelagency/rest/GeographyResource.java
+++ src/main/java/com/travelagency/rest/GeographyResource.java
@@ -7,7 +7,6 @@ import com.travelagency.model.Country;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/geo")
