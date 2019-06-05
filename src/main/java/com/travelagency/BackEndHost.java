6c2dd78671756e97972f5540ec43ac447db1cb93
Rick van der Heijden <rick.van.der.heijden@philips.com>
2019-06-05 23:48:44 +0200

    Fix Codacy error

--- src/main/java/com/travelagency/BackEndHost.java
+++ src/main/java/com/travelagency/BackEndHost.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 @SpringBootApplication(scanBasePackages = "com.travelagency")
 @EnableJpaRepositories(basePackages = "com.travelagency.repository")
 public class BackEndHost implements WebMvcConfigurer {
